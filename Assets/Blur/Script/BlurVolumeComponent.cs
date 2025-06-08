602e2badc1dde70298799a8cfa6beb3766cd93d3
SongRick <18730271290@163.com>
2025-06-08 11:14:06 +0800

    暂停菜单，修改字体中

--- Assets/Blur/Script/BlurVolumeComponent.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine.Rendering;
-
-public class BlurVolumeComponent : VolumeComponent
-{
-    public BoolParameter isActive = new BoolParameter(true);
-    public ClampedFloatParameter blurRadius = new ClampedFloatParameter(0.003f, 0.0f, 0.005f);
-    public ClampedIntParameter iteration = new ClampedIntParameter(3, 1, 4);
-}
